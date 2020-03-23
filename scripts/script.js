e9d01791f739a969073229bd82b84318026a326d
Mila28-06 <milamuijselaar@gmail.com>
2020-03-23 21:12:08 +0100

    commentaar+laatste aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,23 @@
 
+
 setInterval(draaien, 1);
-setInterval(verandering,1) //nog aanpassen
+setInterval(verandering,1);
 
 
+//Door deze functie draaien de wijzers
 function draaien(){
 	var vandaag = new Date();
-	var uur = vandaag.getHours() %12 * 30;
-	var minuut = vandaag.getMinutes() * 6;
-	var seconde = vandaag.getSeconds() * 6;	
+	var uur = vandaag.getHours() %12 * 30; // 12 uren op een ronde klok, 1 uur is 30 graden
+	var minuut = vandaag.getMinutes() * 6; // 1 minuut is 6 graden 
+	var seconde = vandaag.getSeconds() * 6;	// 1 seconde is 6 graden 
 
+	//Hierdoor word het aantal uur/minuten/seconden is graden gekoppeld de rotatie van de wijzers. 
 	document.getElementById("uren").style.transform="rotate("+ uur+ "deg)";
 	document.getElementById("minuten").style.transform="rotate("+ minuut+ "deg)";
 	document.getElementById("seconden").style.transform="rotate("+ seconde+ "deg)";
 }
 
+//Door deze functie veranderd de achtergrond voor de goede fase van de dag
 function verandering(){
 	var vandaag = new Date();
 	var uur = vandaag.getHours();
@@ -21,42 +25,42 @@ function verandering(){
 	var seconde = vandaag.getSeconds();	
 	var body = document.body;
 
-if ("0"<= seconde&&seconde < "15") {
-	document.getElementById("p1").innerHTML = "Goede morgen!";
-	body.classList.add("ochtend");
-	body.classList.remove("nacht");
-}
+	//Hier word afhankelijk van welk uur het is een achtergrond toegevoegd
+	if ("6"<= uur&&uur < "12") {
+		document.getElementById("p1").innerHTML = "Goede morgen";
+		body.classList.add("ochtend");
+		body.classList.remove("nacht");
+	}
 
- else if ("15"<= seconde&&seconde < "30") {
- 	document.getElementById("p1").innerHTML = "Goede middag!";
- 	body.classList.add("middag");
- 	body.classList.remove("ochtend");
-} 
+	 else if ("12"<= uur&&uur < "18") {
+	 	document.getElementById("p1").innerHTML = "Goede middag";
+	 	body.classList.add("middag");
+	 	body.classList.remove("ochtend");
+	} 
 
- else if ("30"<= seconde&&seconde < "45") {
- 	document.getElementById("p1").innerHTML = "Goede avond!";
- 	body.classList.add("avond");
- 	body.classList.remove("middag");
-} 
+	 else if ("18"<= uur) {
+	 	document.getElementById("p1").innerHTML = "Goede avond";
+	 	body.classList.add("avond");
+	 	body.classList.remove("middag");
+	} 
 
-else {
-    console.log("Have a good night!");
-    document.getElementById("p1").innerHTML = "Goede nacht!";
-    body.classList.add("nacht");
-    body.classList.remove("avond");
-}
+	else if ("0"<= uur&&uur < "6"){
+	    document.getElementById("p1").innerHTML = "Goede nacht";
+	    body.classList.add("nacht");
+	    body.classList.remove("avond");
+	}
 
-//Toevoeging tekst 'wakker worden' of 'slaaplekker' bij de aangegeven tijd
+	//Toevoeging tekst 'wakker worden' of 'slaaplekker' bij de aangegeven uur
 
-if ("0"<= seconde&&seconde < "5") {
-	document.getElementById("p2").innerHTML = "Wakker worden!";
-}
+	if ("7"<= uur&&uur < "8") {
+		document.getElementById("p2").innerHTML = "Wakker worden!";
+	}
 
- else if ("30"<= seconde&&seconde < "35") {
- 	document.getElementById("p2").innerHTML = "Slaaplekker!";
-} 
-else{
-	document.getElementById("p2").innerHTML = "";
-}
+	 else if ("21"<= uur&&uur < "23") {
+	 	document.getElementById("p2").innerHTML = "Slaaplekker!";
+	} 
+	else{
+		document.getElementById("p2").innerHTML = "";
+	}
 
 }
